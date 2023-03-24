45020322342ec3f554db85fb5e9bb5f63a1b256a
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-03-24 23:42:34 +0200

    UPD: Added sorting filter (order by) by name/price (asc/desc)
    UPD: Added sorting filter (order by) by name/price (asc/desc)

--- WebShopNet/Core/Specifications/ProductsWithTypesAndBrands.cs
+++ WebShopNet/Core/Specifications/ProductsWithTypesAndBrands.cs
@@ -5,10 +5,26 @@ namespace Core.Specifications;
 
 public class ProductsWithTypesAndBrands : BaseSpecification<Product>
 {
-    public ProductsWithTypesAndBrands()
+    public ProductsWithTypesAndBrands(string? sort)
     {
         AddInclude(x => x.ProductType);
         AddInclude(x => x.ProductBrand);
+
+        if (!string.IsNullOrEmpty(sort))
+        {
+            switch (sort)
+            {
+                case "priceAsc":
+                    AddOrderBy(x => x.Price);
+                    break;
+                case "priceDesc":
+                    AddOrderByDescending(x => x.Price);
+                    break;
+                default:
+                    AddOrderBy(x => x.Name);
+                    break;
+            }
+        }
     }
 
     public ProductsWithTypesAndBrands(int id) : base(x => x.Id == id)
