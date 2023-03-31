b6d21a03e6eced54f706436e819f36eb2c96d65e
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-04-01 01:06:21 +0300

    UPD: Styling client
    UPD: Styling client

--- WebShopNet/client/src/app/app.component.ts
+++ WebShopNet/client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment'
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private baseUrl = environment.apiUrl;
+  
   title = 'SQB';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('request completed')
+    });
+  }
 }
